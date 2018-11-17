0694d3abf99b4142a49e3f50ee777800054285db
aixenhuh <aixenhuh@gmail.com>
2018-11-17 20:40:46 +0900

    스프링 설정 20181117

--- webProject/src/main/java/com/project/web/HomeController.java
+++ webProject/src/main/java/com/project/web/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.common.LoggerInterceptor;
 
 /**
  * Handles requests for the application home page.
@@ -34,6 +38,24 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "index";
+	}
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/introduce", method = RequestMethod.GET)
+	public String introduce(Locale locale, Model model) {
+		Log log = LogFactory.getLog(LoggerInterceptor.class);
+		log.debug("===================================");
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "introduce";
 	}
 	
 }
